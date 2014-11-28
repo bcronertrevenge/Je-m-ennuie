dd828f39b4e0b06047bdc034b7ba3bab49f14448
vilal <lalandev@free.fr>
2014-11-28 14:45:48 +0100

    Ajout de données dans la database. Remplacement de .sqbpro et .sql. ATTENTION .sqlite3 n'est pas à jour

--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,6 @@ CREATE TABLE "Question" (
 	`id_question`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`text`	TEXT
 );
-
 INSERT INTO `Question` VALUES(0,'Est-ce que tu veux être en intérieur ?');
 INSERT INTO `Question` VALUES(1,'Veux tu mettre de l''argent dans ton activité ?');
 INSERT INTO `Question` VALUES(2,'Veux tu dépenser de l''argent ?');
@@ -20,8 +19,6 @@ INSERT INTO `Question` VALUES(12,'Veux tu de détendre ?');
 INSERT INTO `Question` VALUES(13,'Es-tu sur les nerfs ?');
 INSERT INTO `Question` VALUES(14,'Est-ce qu''il fait beau ?');
 INSERT INTO `Question` VALUES(15,'Est-ce qu''il pleut ?');
-
-
 CREATE TABLE `Answer` (
 	`id_answer`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`text1`	TEXT,
@@ -31,7 +28,12 @@ CREATE TABLE `Answer` (
 INSERT INTO `Answer` VALUES(0,'Oui','Carrément','Ok');
 INSERT INTO `Answer` VALUES(1,'Non','Tu rêves','No way !');
 INSERT INTO `Answer` VALUES(2,'Peu importe','Je m''en fiche','Je passe');
-
+CREATE TABLE `ActivityQuestion` (
+	`id_activity`	INTEGER,
+	`id_question`	INTEGER,
+	`impact`	INTEGER
+);
+INSERT INTO `ActivityQuestion` VALUES(0,0,0);
 CREATE TABLE `Activity` (
 	`id_activity`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`text`	TEXT,
@@ -49,24 +51,17 @@ INSERT INTO `Activity` VALUES(6,'Prends ton plus beau crayon et
 dessine moi un mouton',0,0);
 INSERT INTO `Activity` VALUES(7,'Dessine la première chose
  qui te traverse l''esprit',0,0);
- INSERT INTO `Activity` VALUES(8,'Repeins le mur de ta chambre couleur 
+INSERT INTO `Activity` VALUES(8,'Repeins le mur de ta chambre couleur 
 taupe (c''est Valérie Damidot qui l''a dit)',0,0);
- INSERT INTO `Activity` VALUES(9,'Prends toi pour Baudelaire en écrivant un
+INSERT INTO `Activity` VALUES(9,'Prends toi pour Baudelaire en écrivant un
 poème pour ton amoureux(se) ...ou alors ta mère',0,0);
- INSERT INTO `Activity` VALUES(10,'Brode une écharpe pour ta grand-mère',0,0);
- INSERT INTO `Activity` VALUES(11,'Répare les chaussettes trouées que tu
+INSERT INTO `Activity` VALUES(10,'Brode une écharpe pour ta grand-mère',0,0);
+INSERT INTO `Activity` VALUES(11,'Répare les chaussettes trouées que tu
 caches au fond de ton tiroir',0,0);
- INSERT INTO `Activity` VALUES(12,'Construis la maison de tes rêves en
-jouant au Sims 3',0,0);
-
-
-CREATE TABLE `ActivityQuestion` (
-	`id_activity`	INTEGER,
-	`id_question`	INTEGER,
-	`impact`	INTEGER
-);
-
-INSERT INTO `ActivityQuestion` VALUES(0,0,0);
-
-
+INSERT INTO `Activity` VALUES(12,'Construis la maison de tes rêves en jouant au Sims 3',0,0);
+INSERT INTO `Activity` VALUES(13,'Promène toi sur les toits de Paris et défonce des gardes en jouant à Assassin''s Creed',0,0);
+INSERT INTO `Activity` VALUES(14,'Reprend ce fameux projet que tu as laissé dans un coin, la proscrastination c''est pas bon',0,0);
+INSERT INTO `Activity` VALUES(15,'Planifie enfin ce voyage au Bahamas dont tu rêves depuis 3 ans',0,0);
+INSERT INTO `Activity` VALUES(16,'Prépare ton road-trip en Amérique ! L''Amérique tu veux la voir...et tu l''auras',0,0);
+INSERT INTO `Activity` VALUES(17,'Joue au monopoly avec tes petits cousins',0,0);
 COMMIT;
