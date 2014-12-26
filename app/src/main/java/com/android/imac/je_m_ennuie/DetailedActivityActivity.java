b15cc0f30cebfd9a8e10461e4ca9222ad5dae12b
calimarie94 <marie.huertas.pro@gmail.com>
2014-12-26 20:28:24 +0100

    ADD changement de questions au clic sur un bouton dans game

--- app/src/main/java/com/android/imac/je_m_ennuie/DetailedActivityActivity.java
+++ app/src/main/java/com/android/imac/je_m_ennuie/DetailedActivityActivity.java
@@ -1,9 +1,12 @@
 package com.android.imac.je_m_ennuie;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 /**
@@ -16,10 +19,34 @@ public class DetailedActivityActivity extends Activity {
         setContentView(R.layout.detailed_activity);
 
          /* Loading the fonts */
-        TextView title_activity = (TextView) findViewById(R.id.text_activity);
+        TextView title_activity = (TextView) findViewById(R.id.title_activity_detailed);
+        Button btn_facebook = (Button) findViewById(R.id.btn_facebook);
+        Button btn_twitter = (Button) findViewById(R.id.btn_twitter);
+        Button btn_gmail = (Button) findViewById(R.id.btn_gmail);
+        Button btn_favorite = (Button) findViewById(R.id.btn_favorite);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
 
         title_activity.setTypeface(font);
+        btn_favorite.setTypeface(font);
+
+        /* Click events */
+        btn_facebook.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Partage Facebook");
+            }
+        });
+
+        btn_twitter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Partage Twitter");
+            }
+        });
+
+        btn_gmail.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Partage Google");
+            }
+        });
     }
 }
